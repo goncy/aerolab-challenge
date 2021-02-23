bc2af8956431be1860b071af9a3ec191d7614500
goncy <gonzalo.pozzo4@gmail.com>
2021-02-23 18:53:50 -0300

    Update config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
-import reactRefresh from '@vitejs/plugin-react-refresh'
-import { defineConfig } from 'vite'
+import path from "path";
+
+import reactRefresh from "@vitejs/plugin-react-refresh";
+import {defineConfig} from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [reactRefresh()]
-})
+  plugins: [reactRefresh()],
+  alias: [
+    {
+      find: "~",
+      replacement: path.resolve(path.resolve(__dirname), "src"),
+    },
+  ],
+});
