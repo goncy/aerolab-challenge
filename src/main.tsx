d5fe214b0de1398469fd5e86d6ba13fd85614aa1
goncy <gonzalo.pozzo4@gmail.com>
2021-04-07 12:52:39 -0300

    Update eslint

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom";
 import {ChakraProvider} from "@chakra-ui/react";
 
+import theme from "./theme";
+
 import HomeScreen from "~/app/screens/Home";
 import Layout from "~/app/components/Layout";
 import {Provider as UserProvider} from "~/user/context";
-
-import theme from "./theme";
 
 import "./theme.css";
 
